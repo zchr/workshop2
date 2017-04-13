60d6668193eac853cba06f9dfd71949c600ef32d
Nick Moolenijzer <nick@moolenijzer.com>
2017-04-13 11:53:47 -0400

    remove js stuff and add js script

--- js/init.js
+++ js/init.js
@@ -4,11 +4,4 @@
 // this initializes all modals so they can be triggered
 $(document).ready(function() {
   //initializing code goes here!
-  $(".dropdown-button").dropdown({ hover: false });
-  // code from:http://materializecss.com/modals.html#!
-  // additional help from: http://stackoverflow.com/questions/41626343/materialized-modal-not-displaying
-  // this initializes all modals so they can be triggered
-  $('.modal').modal();
-  // parallax code from: http://materializecss.com/parallax.html
-  $('.parallax').parallax();
 });
